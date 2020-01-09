a41ea33b1703d2ca9d6a6c46d1a7d6a09cb3d5b2
Alex Hanrahan <satoruhanrahan@hotmail.com>
2020-01-09 23:21:38 +0900

    Update kanji.js

--- server/routes/kanji.js
+++ server/routes/kanji.js
@@ -4,24 +4,24 @@ const verify = require('../verifyToken');
 
 router.get('/', verify, async (req, res) => {
   const N5kanji = await loadKanjiCollection()
-    .then(()=>{
+    .then(() => {
       res.send(await N5kanji.find({}).toArray());
     })
-    .catch(function(err) {
+    .catch(function (err) {
       res.send(err);
     })
 });
 
 //Connect to DB
 async function loadKanjiCollection() {
-  try{
+  try {
     const client = await mongodb.MongoClient.connect(process.env.DB_CONNECT,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true
       });
     return client.db('KanjiStories').collection('N5Kanji');
-  } catch(err){
+  } catch (err) {
     return err;
   }
 }
