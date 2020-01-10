3d5c1062278177fa35f769eb529fb00288c4894c
Alex Hanrahan <satoruhanrahan@hotmail.com>
2020-01-10 12:19:02 +0900

    Update stories.js

--- server/routes/stories.js
+++ server/routes/stories.js
@@ -59,7 +59,7 @@ router.post('/insert', async (req, res) => {
 });
 
 //Connect to DB and get stories
-async function loadStories() {
+function loadStories() {
   try {
     // const client = await mongodb.MongoClient.connect("mongodb+srv://satoruhanrahan:ygshP%24_ubC%26Ee3a@cluster0-l7ajy.gcp.mongodb.net/stories?ssl=true&authSource=admin&w=majority",
     //   {
@@ -85,6 +85,7 @@ async function loadStories() {
       const collection = client.db("KanjiStories").collection("stories");
       const stories = collection.find({}).toArray();
       client.close();
+      console.log("stories", stories)
       return stories;
     });
   } catch (err) {
