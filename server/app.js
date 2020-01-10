ac0b10a81b502002ba6c3a5459fbd64633770181
Alex Hanrahan <satoruhanrahan@hotmail.com>
2020-01-10 15:02:44 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,10 +6,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 //Connect to DB
-// mongoose.connect(process.env.DB_CONNECT,
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => console.log('connected to db!')
-// );
+mongoose.connect(process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log('connected to db!')
+);
 //Import Routes
 const authRoute = require('./routes/auth');
 const kanjiRoute = require('./routes/kanji');
